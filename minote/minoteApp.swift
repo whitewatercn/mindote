d755fe62fe8e7a2dd8cd4c4ffdba75e6ddba0b09
whitewatercn <whitewatercn@Outlook.com>
2025-06-20 16:35:37 +0800

    Initial Commit

---
+++ minote/minoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  minoteApp.swift
+//  minote
+//
+//  Created by whitewater on 6/20/25.
+//
+
+import SwiftUI
+
+@main
+struct minoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
